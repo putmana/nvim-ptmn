52dc2c2af67a4491f0d638985cc1fcd1df05b143
putmana <adam@ptmn.io>
2023-12-03 02:47:28 -0700

    Changed harpoon config

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -10,13 +10,13 @@ return {
     harpoon:setup()
     -- REQUIRED
 
-    vim.keymap.set("n", "\\", function() harpoon:list():append() end)
-    vim.keymap.set("n", "<leader>he", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+    vim.keymap.set("n", "\\h", function() harpoon:list():append() end)
+    vim.keymap.set("n", "\\e", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-    vim.keymap.set("n", "<leader>ha", function() harpoon:list():select(2) end)
-    vim.keymap.set("n", "<leader>hs", function() harpoon:list():select(2) end)
-    vim.keymap.set("n", "<leader>hd", function() harpoon:list():select(3) end)
-    vim.keymap.set("n", "<leader>hf", function() harpoon:list():select(4) end)
+    vim.keymap.set("n", "\\a", function() harpoon:list():select(1) end)
+    vim.keymap.set("n", "\\s", function() harpoon:list():select(2) end)
+    vim.keymap.set("n", "\\d", function() harpoon:list():select(3) end)
+    vim.keymap.set("n", "\\f", function() harpoon:list():select(4) end)
   end,
 
   opts = {}
