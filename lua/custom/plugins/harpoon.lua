6fab185f6c5559ff6745afa24f3227b900dffec5
putmana <adam@ptmn.io>
2023-12-03 00:46:30 -0700

    Installed harpoon

---
+++ lua/custom/plugins/harpoon.lua
@@ -0,0 +1,23 @@
+return {
+  "ThePrimeagen/harpoon",
+  branch = "harpoon2",
+  dependencies = { "nvim-lua/plenary.nvim" },
+
+  config = function()
+    local harpoon = require("harpoon")
+
+    -- REQUIRED
+    harpoon:setup()
+    -- REQUIRED
+
+    vim.keymap.set("n", "\\", function() harpoon:list():append() end)
+    vim.keymap.set("n", "<leader>he", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+    vim.keymap.set("n", "<leader>ha", function() harpoon:list():select(2) end)
+    vim.keymap.set("n", "<leader>hs", function() harpoon:list():select(2) end)
+    vim.keymap.set("n", "<leader>hd", function() harpoon:list():select(3) end)
+    vim.keymap.set("n", "<leader>hf", function() harpoon:list():select(4) end)
+  end,
+
+  opts = {}
+}
